c9853675ace3f5ebb8b6b1121d0d424649b469d4
kishalaya1 <kishalaya1@yahoo.com>
2022-11-25 22:53:07 +0530

    component example complete
    component example in asp.net core 7

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ScholarHat.Models;
+using System;
 
 namespace ScholarHat.Controllers
 {
@@ -6,7 +9,19 @@ namespace ScholarHat.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            PostViewModel? _blogPost = new PostViewModel();
+            //get a random post id to display
+            Random _randomPostNo = new Random();  
+            // Any random integer   
+            int _blogPostNo = _randomPostNo.Next(1,100);
+            string _postUrl = "https://jsonplaceholder.typicode.com/posts/" + _blogPostNo;
+            HttpClient _client = new HttpClient();
+            HttpResponseMessage response = _client.GetAsync(_postUrl).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                _blogPost = JsonConvert.DeserializeObject<PostViewModel>(response.Content.ReadAsStringAsync().Result);
+            }
+            return View(_blogPost);
         }
     }
 }
--- Models/PostViewModel.cs
+++ Models/PostViewModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Author { get; set; }
+        public string UserId{ get; set; }
         public string Body { get; set; }
     }
 }
