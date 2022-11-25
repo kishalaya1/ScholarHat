e1e48cf56770580cbf2a4ca93acbda8bfbea606b
kishalaya1 <kishalaya1@yahoo.com>
2022-11-25 19:57:19 +0530

    demo
    View component demo

---
+++ Components/CommentsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ScholarHat.Models;
+
+
+namespace ScholarHat.Components
+{
+    public class CommentsViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(int id)
+        {
+            List<CommentViewModel>? lstComments = new List<CommentViewModel>();
+
+            string url = "https://jsonplaceholder.typicode.com/posts/" + id + "/comments";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                lstComments = JsonConvert.DeserializeObject<List<CommentViewModel>>(response.Content.ReadAsStringAsync().Result);
+            }
+
+            return View("~/Views/Components/_Comments.cshtml", lstComments);
+        }
+    }
+}
---
+++ Models/CommentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ScholarHat.Models
+{
+    public class CommentViewModel
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Body { get; set; }
+
+    }
+}
